c4ecf058beb60e037e322657a03b476d50b2f84f
leabeadeaff <87875395+leabeadeaff@users.noreply.github.com>
2022-09-06 11:09:07 +0300

    Add files via upload

---
+++ progress.js
@@ -0,0 +1,25 @@
+let input = document.getElementById('1');
+let animate = document.getElementById('2');
+let hide = document.getElementById('3');
+let progressBlock = document.querySelector('.progress');
+let circle = document.querySelector('.circle');
+let bar = document.querySelector('.bar')
+let radiusBar = bar.getAttribute('r');
+
+input.oninput = function () {
+    if (Number.isInteger(Number(input.value)) && input.value >= 0 && input.value <= 100) {
+        bar.style.strokeDashoffset = (quickMath(radiusBar) - quickMath(radiusBar) * input.value / 100);
+    }
+}
+
+animate.onclick = () => random() ? progressBlock.classList.toggle('start') : circle.classList.toggle('start');
+hide.onclick = () => progressBlock.style.display = 'none';
+
+function quickMath (radius) {
+    //2+2=4-1=3
+    return radius*2*Math.PI;
+}
+
+function random () {
+    return Math.trunc((Math.random() * 10)) % 2 === 0;
+}
